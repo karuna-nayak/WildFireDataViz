c5cee6536e873faf0bad26131e0393736864ee64
Kriti2885 <42858268+Kriti2885@users.noreply.github.com>
2020-04-06 20:02:37 -0700

    Route and component
    Add route and component

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 // var router = express.Router();
 
 const app =  express();
-const home = require('./routes/home');
+// const home = require('./routes/home');
+const homeRouter = require('./src/routes/homeRoutes');
+app.use('/', homeRouter);
 
-app.use('/', home);
+// app.use('/', home);
 
 
 app.listen(3000);
